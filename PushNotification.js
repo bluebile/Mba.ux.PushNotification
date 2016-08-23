24f789e8d6dea7c052e16ee415910428972d3ced
viniciusgandrade <viniciusgandrade@gmail.com>
2016-08-23 15:45:47 -0300

    extensão plugin pushwoosh

--- PushNotification.js
+++ PushNotification.js
@@ -1,5 +1,10 @@
 Ext.define('Mba.ux.PushNotification', {
 
+    requires: [
+        'Mba.ux.PushAeroGear',
+        'Mba.ux.PushWoosh'
+    ],
+
     constructor: function() {
         var className = 'Mba.ux.PushWoosh';
         if (typeof push === 'undefined') {
